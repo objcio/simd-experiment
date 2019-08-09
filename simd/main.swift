110159595f6e138cd724a61f12a29c946c41247d
Florian Kugler <mail@floriankugler.com>
2019-08-09 13:02:27 +0200

    Better names, fix boundary issues

--- simd/main.swift
+++ simd/main.swift
@@ -8,104 +8,98 @@
 
 import Foundation
 
-extension UInt64: Collection {
+extension UInt64: Collection, CustomDebugStringConvertible {
     public var startIndex: Int { return 0 }
     public var endIndex: Int { return 64 }
+
     public func index(after i: Int) -> Int {
         return i + 1
     }
+
     public subscript(_ idx: Int) -> Int {
         return self & (1 << idx) != 0 ? 1 : 0
     }
-}
 
-extension UInt64: CustomDebugStringConvertible {
     public var debugDescription: String {
         return map(String.init).joined()
     }
+}
 
-    func oneIndices(offset: Int = 0) -> [Int] {
-        var result: [Int] = []
-        result.reserveCapacity(16)
+extension UInt64 {
+    func oneIndices(offset: Int = 0, appendTo result: inout [Int]) {
         for idx in 0..<64 {
             if self & 1 << idx != 0 {
                 result.append(idx + offset)
             }
         }
-        return result
     }
 }
 
-extension Data {
-    func paddedPrefix(_ length: Int) -> Data {
-        var chunk = prefix(length)
-        if chunk.count < length {
-            chunk += Data(repeating: 0, count: length-chunk.count)
-        }
-        return chunk
-    }
-}
+let evens: UInt64 = (0...63).reduce(0) { result, idx in idx % 2 == 0 ? result | (1 << idx) : result }
+let odds = ~evens
 
 func controlCharacterMask(for data: UnsafePointer<UInt8>, inQuotes: inout Bool) -> (commas: UInt64, newlines: UInt64) {
-    let q = cmp_mask_against_input(data, 34)
-    let c = cmp_mask_against_input(data, 44)
-    let n = cmp_mask_against_input(data, 10)
+    let quotes = cmp_mask_against_input(data, 34)
+    let commas = cmp_mask_against_input(data, 44)
+    let newlines = cmp_mask_against_input(data, 10)
 
-    let s = q & ~(q << 1)
+    let quoteStarts = quotes & ~(quotes << 1)
+    
 
-    let e: UInt64 = (0...63).reduce(0) { result, idx in idx % 2 == 0 ? result | (1 << idx) : result }
-    let o = ~e
 
-    let es = s & e
-    let ec = q &+ es
-    let ece = ec & ~q
-    let od1 = ece & ~e
+    let evenQuoteStarts = quoteStarts & evens
+    let evenQuotesWithCarries = quotes &+ evenQuoteStarts
+    let evenQuoteCarries = evenQuotesWithCarries & ~quotes
+    let oddQuoteEndsWithEvenStarts = evenQuoteCarries & ~evens
 
-    let os = s & o
-    let oc = q &+ os
-    let oce = oc & ~q
-    let od2 = oce & e
+    let oddQuoteStarts = quoteStarts & odds
+    let (oddQuotesWithCarries, overflow) = quotes.addingReportingOverflow(oddQuoteStarts)
+    let oddQuoteCarries = oddQuotesWithCarries & ~quotes
+    let evenQuoteEndsWithOddStarts = oddQuoteCarries & evens
 
-    let od = od1 | od2
+    let quoteEnds = oddQuoteEndsWithEvenStarts | evenQuoteEndsWithOddStarts
 
-    let stringMask = carryless_multiply(od, ~0)
+    let stringMask = carryless_multiply(quoteEnds, ~0)
+    let controlMask = ~stringMask
+    let finalMask = inQuotes ? ~controlMask : controlMask
 
-    let finalMask = inQuotes ? stringMask : ~stringMask
-    let cq = c & finalMask
-    let nq = n & finalMask
-    if cq.nonzeroBitCount % 2 != 0 {
-        inQuotes.toggle()
-    }
-    return (cq, nq)
+    let controlCommas = commas & finalMask
+    let controlNewlines = newlines & finalMask
+
+    let endsInString = stringMask & (1 << 63) != 0
+    inQuotes = endsInString != overflow
+    return (controlCommas, controlNewlines)
 }
 
 
 public func parseCSV(data: Data) -> [[String]] {
     var commas: [Int] = []
-    commas.reserveCapacity(1000)
     var newlines: [Int] = []
-    newlines.reserveCapacity(1000)
     var inQuotes = false
-
+    
     data.withUnsafeBytes { buf in
         for start in stride(from: 0, to: buf.count, by: 64) {
             let end = min(buf.count, start + 64)
-            var ptr = buf.baseAddress!.assumingMemoryBound(to: UInt8.self) + start
-            if end - start < 64 {
-                let new = UnsafeMutablePointer<UInt8>.allocate(capacity: 64)
-                new.initialize(repeating: 0, count: 64)
-                new.assign(from: buf.baseAddress!.assumingMemoryBound(to: UInt8.self) + start, count: end - start)
-                ptr = UnsafePointer(new)
+            let ptr = buf.baseAddress!.assumingMemoryBound(to: UInt8.self) + start
+            let result: (commas: UInt64, newlines: UInt64)
+            let size = end - start
+            if size < 64 {
+                var foo = Data(repeating: 0, count: 64)
+                foo.replaceSubrange(0..<size, with: ptr, count: size)
+                result = foo.withUnsafeBytes { bytes in
+                    controlCharacterMask(for: bytes.baseAddress!.assumingMemoryBound(to: UInt8.self), inQuotes: &inQuotes)
+                }
+            } else {
+                result = controlCharacterMask(for: ptr, inQuotes: &inQuotes)
             }
-            let result = controlCharacterMask(for: ptr, inQuotes: &inQuotes)
-            commas.append(contentsOf: result.commas.oneIndices(offset: start))
-            newlines.append(contentsOf: result.newlines.oneIndices(offset: start))
+            result.commas.oneIndices(offset: start, appendTo: &commas)
+            result.newlines.oneIndices(offset: start, appendTo: &newlines)
         }
     }
-
+    
     var commas2 = commas[...] + [data.count]
     var result: [[String]] = []
-    result.reserveCapacity(1000)
+    result.reserveCapacity(32)
     var lineStart = 0
     for lineEnd in newlines + [data.count] {
         guard lineStart < lineEnd else { break }
@@ -134,13 +128,13 @@ func measure<A>(name: String = "", _ block: () -> A) -> A {
     return result
 }
 
-let csv = #"""
-"Fiel,d 1","Field with ""quotes""","""""qu,otes""""","kajhsdkahdakshdkajdhkj",",,,,"
-"Another line", "with , commas, in fields", "and ""escaped quotes"""
-"""#
-//let data = csv.data(using: .ascii)!
-let data = try! Data(contentsOf: URL(fileURLWithPath: "/Users/florian/Downloads/stops.txt"))
+//let csv = #"""
+//"Fiel,d 1",,"Field with ""quotes""","""""qu,otes""""","kajhsdaa",
+//"Another line", "with , commas, in fields", "and ""escaped quotes"""
+//"""#
+//let data = csv.data(using: .utf8)!
+//dump(parseCSV(data: data))
+let data = try! Data(contentsOf: URL(fileURLWithPath: "/Users/florian/Downloads/shapes.txt"))
 measure {
     _ = parseCSV(data: data)
 }
-//dump(parseCSV(data: data))
